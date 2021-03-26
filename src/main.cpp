d7ffa36304661939eb00a2bb8bbf8b2141fa91e8
mdevana <madhusudhan.d@gmail.com>
2021-03-26 16:05:30 +0100

    first code

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-   steer_pid.Init(0.1,0,0);
+   pid.Init(0.1,0,0);
    speed_pid.Init(0.1,0,0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
