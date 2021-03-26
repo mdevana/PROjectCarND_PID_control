00e16dc2045a4bc2e8127c0db54e1813c35c4a9f
mdevana <madhusudhan.d@gmail.com>
2021-03-26 16:12:12 +0100

    first code

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main() {
    pid.Init(0.1,0,0);
    speed_pid.Init(0.1,0,0);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  h.onMessage([&pid,&speed_pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
