5ac4d469d0e4d8d6b1afc514f4971eb005841505
mdevana <madhusudhan.d@gmail.com>
2021-03-26 16:14:26 +0100

    first code

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
-   pid.Init(0.1,0,0);
+   pid.Init(0.1,0,0.1);
    speed_pid.Init(0.1,0,0);
 
   h.onMessage([&pid,&speed_pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
