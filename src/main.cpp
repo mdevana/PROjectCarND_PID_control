6f5c222a9a5edeff865e62acf1e3f81c5d82eec8
mdevana <madhusudhan.d@gmail.com>
2021-03-26 16:00:15 +0100

    first code

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,13 @@ string hasData(string s) {
 int main() {
   uWS::Hub h;
 
-  PID pid;
+  PID steer_pid;
+  PID speed_pid;
   /**
    * TODO: Initialize the pid variable.
    */
-   pid.Init(0.1,0.1,0.1);
+   steer_pid.Init(0.1,0,0);
+   speed_pid.Init(0.1,0,0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -64,14 +66,18 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-		   pid.UpdateError(cte);
+		   steer_pid.UpdateError(cte);
            steer_value = pid.CalculateSteerValue();
+		   speed_pid.UpdateError(cte);
+           speed_value = pid.CalculateSteerValue();
+		   
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value<<" Speed Value: " <<speed_value
                     << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
+		  msgJson["speed"] = speed_value;
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
