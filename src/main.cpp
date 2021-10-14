7a32a60584e68de6242fcdbf8961bf6b11e0c62a
mdevana <madhusudhan.d@gmail.com>
2021-10-14 20:20:37 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ int main() {
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-		  msgJson["speed"] = speed_value;
+		  msgJson["speed"] = 30;
           msgJson["throttle"] = throttle_value;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           //std::cout << msg << std::endl;
